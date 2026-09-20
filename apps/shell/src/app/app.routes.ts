b2962999d426445a0861dc060b365745f53d0ce8
LucasAlNascimento <lucasalvesnascimento.lan@gmail.com>
2026-09-20 12:33:48 -0300

    feat: integrate catalog remote with shell

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+	{
+		path: 'catalog',
+		loadComponent: () =>
+			loadRemoteModule('catalog', './Component').then((m) => m.App),
+	},
+];
